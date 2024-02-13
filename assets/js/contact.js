041e820b8f31ebe6a1137ef255078037b2e61d04
PhanChauSongAnh~ <tbc.problox2k7@gmail.com>
2024-02-13 16:26:35 +0700

    up

---
+++ assets/js/contact.js
@@ -0,0 +1,5 @@
+//validation form
+var Username = document.getElementById("Name").value
+var Email = document.getElementById("Mail").value
+var phonenum = document.getElementById("Phone").value
+var textmess =  document.getElementById("Message").value
